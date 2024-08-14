d4065aeb5ee29e9f0522dca936d9b931f26e0a9f
ayushgitt <112325172+ayushgitt@users.noreply.github.com>
2024-08-14 16:39:56 +0530

    Add files via upload

---
+++ src/components/Workcardata.js
@@ -0,0 +1,26 @@
+import pro1 from "../images/proj1.jpeg"
+
+const Projectdata=[
+    {
+        imgsrc:pro1,
+        title:"PORTFOLIO",
+        text:"",
+        view:"google.com"
+    },
+    {
+        imgsrc:pro1,
+        title:"HEALTH PREDICTION",
+        text:"",
+        view:"google.com"
+        
+    },
+    {
+        imgsrc:pro1,
+        title:"CODE COLLAB",
+        text:"",
+        view:"google.com"
+    },
+
+]
+
+export default Projectdata
